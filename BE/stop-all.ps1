a0509bfce52651c13f32265858285d356bfbb305
Vietnguyen203 <nguyenlanviet09@gmail.com>
2026-04-22 15:50:16 +0700

    bảng DB liquibase

--- BE/stop-all.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "SilentlyContinue"
-Write-Host "Stopping all Java processes..." -ForegroundColor Yellow
-Get-Process -Name "java" | Stop-Process -Force
-Write-Host "All Java processes stopped!" -ForegroundColor Green
